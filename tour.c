e28a6aa274cb37dac5d0bb69880872cb85132664
upasi mehta <upasi.mehta@stonybrook.edu>
2015-11-28 21:17:01 -0500

    Creating list of IPs

--- tour.c
+++ tour.c
@@ -27,14 +27,102 @@ int create_udp_socket() {
 	return 0;
 }
 
-void main() {
+void get_my_vm() {
+	gethostname(my_vm, 5);
+
+}
+
+int check_tour(int argc, char **argv) {
+	int i=0;
+	char prev_vm[5];
+	struct hostent *host;
+
+	strcpy(prev_vm, "");
+
+	while(i != argc) {
+		if(i != 0) {
+			if(i == 1 && strcmp(argv[i],my_vm) == 0) {
+				printf("Invalid input \n");
+				return -1;
+			}
+
+			if(strncmp(argv[i],"vm",2) != 0) {
+				printf("Invalid input \n");
+				return -1;
+			}
+
+			if((host = gethostbyname(argv[i])) == NULL) {
+				printf("Invalid input \n");
+				return -1;
+			}
+
+			if(strcmp(prev_vm, argv[i]) == 0) {
+				printf("Invalid input \n");
+				return -1;
+			}
+			strcpy(prev_vm, argv[i]);
+		}
+		i++;
+	}
+	return 0;
+}
+
+void create_ip_list(int argc, char **argv) {
+	
+	int i=0;
+	struct hostent *host;
+	struct in_addr **addr_list;
+	
+	host = gethostbyname(my_vm);
+	addr_list = (struct in_addr **)host->h_addr_list;	
+
+	ip_head = malloc(sizeof(struct ip_list));
+	strcpy(ip_head->ip_addr, inet_ntoa(*addr_list[0]));
+	ip_head->ip_next = NULL;
+
+	while(i != argc) {
+		if(i != 0) {
+			
+			host = gethostbyname(argv[i]);
+			addr_list = (struct in_addr **)host->h_addr_list;
+
+			struct ip_list *new_ip = malloc(sizeof(struct ip_list));
+			strcpy(new_ip->ip_addr, inet_ntoa(*addr_list[0]));
+			new_ip->ip_next = NULL;
+
+			struct ip_list *ip_temp = ip_head;
+			while(ip_temp->ip_next != NULL) {
+				ip_temp = ip_temp->ip_next;
+			}
+			ip_temp->ip_next = new_ip;
+		}
+		i++;
+	}
+}
+
+void print_ip_list() {
+	struct ip_list *ip_temp = ip_head;
+	while(ip_temp != NULL) {
+		printf("IP is %s \n",ip_temp->ip_addr);
+		ip_temp = ip_temp->ip_next;
+	}
+}
+
+void main(int argc, char **argv) {
 	
 	int rt_sockfd = create_rt_socket();
 	int pg_sockfd = create_pg_socket();
 	int pf_sockfd = create_pf_pack_socket();
 	int udp_sockfd = create_udp_socket();
 
-	printf("rt socket returned is %d \n", rt_sockfd);
-	printf("pg socket returned is %d \n", pg_sockfd);
-	printf("pf packet socket returned is %d \n", pf_sockfd);
+	get_my_vm();
+
+	// Check whether the order is valid or not and check the vm name too
+	if(check_tour(argc, argv)) {
+		exit(0);
+	}
+
+	create_ip_list(argc, argv);
+	print_ip_list();
+
 }
--- tour.h
+++ tour.h
@@ -4,3 +4,11 @@
 #include <linux/if_ether.h>
 
 #define RT_PROTOCOL 15526
+
+struct ip_list {
+	char ip_addr[30];
+	struct ip_list *ip_next;
+};
+
+char my_vm[5];
+struct ip_list *ip_head;
