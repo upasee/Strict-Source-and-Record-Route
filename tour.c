00c3b8ae999815fe3cd4777ee571c06b5def8c66
upasi mehta <upasi.mehta@stonybrook.edu>
2015-11-30 04:35:54 -0500

    Chaging IP list format

--- tour.c
+++ tour.c
@@ -18,18 +18,12 @@ int create_pg_socket() {
 
 int create_pf_pack_socket() {
 	int sockfd;
-	sockfd = Socket(PF_PACKET, SOCK_RAW, ETH_P_IP);
+	sockfd = Socket(PF_PACKET, SOCK_RAW, htons(ETH_P_IP));
 	return sockfd;
-}
-
-int create_udp_socket() {
-	int sockfd;
-	return 0;
 }
 
 void get_my_vm() {
 	gethostname(my_vm, 5);
-
 }
 
 int check_tour(int argc, char **argv) {
@@ -67,7 +61,7 @@ int check_tour(int argc, char **argv) {
 	return 0;
 }
 
-void create_ip_list(int argc, char **argv) {
+void create_ip_list(struct ip_list *ipl, int argc, char **argv) {
 	
 	int i=0;
 	struct hostent *host;
@@ -76,36 +70,36 @@ void create_ip_list(int argc, char **argv) {
 	host = gethostbyname(my_vm);
 	addr_list = (struct in_addr **)host->h_addr_list;	
 
-	ip_head = malloc(sizeof(struct ip_list));
-	strcpy(ip_head->ip_addr, inet_ntoa(*addr_list[0]));
-	ip_head->ip_next = NULL;
+	ipl->total_ips = argc;
+	ipl->ip_addr = malloc(argc*sizeof(char *));
+	ipl->ip_addr[0] = malloc(sizeof(20*sizeof(char)));
+	strcpy(ipl->ip_addr[0], inet_ntoa(*addr_list[0]));
 
 	while(i != argc) {
 		if(i != 0) {
-			
 			host = gethostbyname(argv[i]);
 			addr_list = (struct in_addr **)host->h_addr_list;
-
-			struct ip_list *new_ip = malloc(sizeof(struct ip_list));
-			strcpy(new_ip->ip_addr, inet_ntoa(*addr_list[0]));
-			new_ip->ip_next = NULL;
-
-			struct ip_list *ip_temp = ip_head;
-			while(ip_temp->ip_next != NULL) {
-				ip_temp = ip_temp->ip_next;
-			}
-			ip_temp->ip_next = new_ip;
+			ipl->ip_addr[i] = malloc(sizeof(20*sizeof(char)));
+			strcpy(ipl->ip_addr[i], inet_ntoa(*addr_list[0]));
 		}
+		i++;
+	}
+	
+	ipl->curr_ip_pos = 0;
+	strcpy(ipl->multicast_addr, MULTICAST_ADDR); 
+	ipl->multicast_port = MULTICAST_PORT;
+}
+
+void print_ip_list(struct ip_list *ipl) {
+	int i=0;
+	while(i != ipl->total_ips) {
+		printf("IP is %s \n", ipl->ip_addr[i]);
 		i++;
 	}
 }
 
-void print_ip_list() {
-	struct ip_list *ip_temp = ip_head;
-	while(ip_temp != NULL) {
-		printf("IP is %s \n",ip_temp->ip_addr);
-		ip_temp = ip_temp->ip_next;
-	}
+void send_tour_packet(int sockfd) {
+	
 }
 
 void main(int argc, char **argv) {
@@ -113,7 +107,11 @@ void main(int argc, char **argv) {
 	int rt_sockfd = create_rt_socket();
 	int pg_sockfd = create_pg_socket();
 	int pf_sockfd = create_pf_pack_socket();
-	int udp_sockfd = create_udp_socket();
+	struct ip_list ipl;
+	int send_mcast_sockfd, recv_mcast_sockfd;
+	struct sockaddr *sasend, *sarecv;
+	socklen_t salen;
+	const int on=1;
 
 	get_my_vm();
 
@@ -122,7 +120,21 @@ void main(int argc, char **argv) {
 		exit(0);
 	}
 
-	create_ip_list(argc, argv);
-	print_ip_list();
+	create_ip_list(&ipl, argc, argv);
+	print_ip_list(&ipl);
 
+	/* Send tour packet */
+	send_tour_packet(rt_sockfd);
+
+	/* UDP Socket */
+	send_mcast_sockfd = Udp_client(MULTICAST_ADDR, MULTICAST_PORT, (void **)&sasend, &salen);
+	recv_mcast_sockfd = Socket(sasend->sa_family, SOCK_DGRAM, 0);
+	Setsockopt(recv_mcast_sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+	sarecv = Malloc(salen);
+	memcpy(sarecv, sasend, salen);
+	Bind(recv_mcast_sockfd, sarecv, salen);
+
+	Mcast_join(recv_mcast_sockfd, sasend, salen, NULL, 0);
+	Mcast_set_loop(send_mcast_sockfd, 0);
 }
--- tour.h
+++ tour.h
@@ -4,11 +4,15 @@
 #include <linux/if_ether.h>
 
 #define RT_PROTOCOL 15526
+#define MULTICAST_PORT 8000
+#define MULTICAST_ADDR "225.0.0.1"
 
 struct ip_list {
-	char ip_addr[30];
-	struct ip_list *ip_next;
+	char **ip_addr;
+	int total_ips;
+	int curr_ip_pos;
+	char multicast_addr[30];
+	int multicast_port;
 };
 
 char my_vm[5];
-struct ip_list *ip_head;
